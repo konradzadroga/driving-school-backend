e1a4203832834750f45b62656ac6bac802850dff
Konrad Zadroga <konradzadroga@pc35.home>
2019-11-08 21:22:27 +0100

    gitignore update

--- target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO roles (name) values ('ROLE_USER');
-INSERT INTO roles (name) values ('ROLE_ADMIN');
-INSERT INTO roles (name) values ('ROLE_DBA');
-
-INSERT INTO users (username, email, password) VALUES ('konrad', 'konrad@mail.com', 'butter');
-
-insert into users_roles (user_id, roles_id) values (1,1);
-insert into users_roles (user_id, roles_id) values (1,2);
